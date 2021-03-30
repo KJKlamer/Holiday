edf0908bec3550b2e8fd9ecbaa75ba0fc6ac27d1
KJKlamer <kklamer@stcharlessd.org>
2021-03-30 13:37:41 -0500

    yes

--- timer.js
+++ timer.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "HAPPY 4TH!";
   }
 }, 1000);
